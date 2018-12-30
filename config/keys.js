1fad86ce05ef009c238ff91cfa90b50184857ffb
Kunle Adeyinka <aadeyink@gmail.com>
2018-12-30 03:49:49 -0500

    added map api
    used mapbox api to add map feature to campground ui

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports =  {
-    mongoURI: 'mongodb://admin:admin007@ds123532.mlab.com:23532/yelpcampdb'
+    mongoURI: 'mongodb://admin:admin007@ds123532.mlab.com:23532/yelpcampdb',
 }; 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 const Campground = require("../models/campground");
 const middleware = require("../middleware");
+
+const url = ''
 
 
 //show all campgrounds
@@ -19,25 +22,55 @@ router.get("/", (req, res) => {
 //add a new campground
 router.post("/", middleware.isLoggedIn,  (req, res) => {
 
-    let campName = req.body.name;
-    let campImage = req.body.image;
-    let description = req.body.description;
-    let price = req.body.price;
-    let author = {
+    var campName = req.body.name;
+    var campImage = req.body.image;
+    var description = req.body.description;
+    var price = req.body.price;
+    var locationArray = [];
+    var author = {
         id: req.user._id,
         username: req.user.username
     };
 
-    let newCampground = { name: campName, price: price, image: campImage, description: description, author: author };
-    Campground.create(newCampground, (err, newlyCreated) => {
-        if(err){
-          console.log(err);  
-        }else{
-            res.redirect("/campgrounds");
+    axios.get("https://api.mapbox.com/v4/geocode/mapbox.places/" + req.body.location + ".json?access_token=pk.eyJ1IjoiYWFkZXlpbmthMDA3IiwiYSI6ImNqcTk0ZWptZTB2YWM0M2s0dm4ycHBhazEifQ.4Lz7IcxUsjLrskOC6z0cPQ")
+    .then(response => {
+        locationArray = response.data.features[0].geometry.coordinates;
+        
+        if(!locationArray.length){
+            req.flash("error", "Invalid Address");
+            return res.redirect("back");
         }
 
+        var lat = locationArray[1];
+        var lng = locationArray[0];
+        var location = response.data.features[0].text;
+        var newCampground = { 
+            name: campName, 
+            price: price, 
+            image: campImage, 
+            description: description, 
+            author: author,
+            location: location,
+            lat: lat,
+            lng: lng 
+        };
+
+        Campground.create(newCampground, (err, newlyCreated) => {
+            if(err){
+              console.log(err);  
+            }else{
+                console.log(newlyCreated);
+                res.redirect("/campgrounds");
+            }
+        });
+    
+    }).catch(error => {
+        console.log(error);
+        req.flash("error", "Invalid Address");
+        return res.redirect("back");
     });
 });
+
 
 // show create new campground form
 router.get("/new", middleware.isLoggedIn,  (req, res) => {
